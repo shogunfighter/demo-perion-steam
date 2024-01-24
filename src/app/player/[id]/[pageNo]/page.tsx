e9fda4de1c9cce9c36695b66721cf487ef8a374c
shogunfighter <rene.cabrera.jr@gmail.com>
2024-01-24 22:26:40 +1100

    fix linting issues

--- src/app/player/[id]/[pageNo]/page.tsx
+++ src/app/player/[id]/[pageNo]/page.tsx
@@ -81,19 +81,19 @@ export default async function Page(args: TPageParam) {
 
                 <nav aria-label="Page navigation example">
                     {pageCount > 1 && <ul className="inline-flex flex-row flex-wrap justify-center items-start -space-x-px text-sm">
-                        <li>
+                        <li key={"prev"}>
                             <Link href={pageNo > 1 ? `/player/${id}/${pageNo - 1}` : "#"} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 ms-0 border-e-0 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</Link>
                         </li>
                         {
-                            Array.from({ length: pageCount }).map((_, index) => <li>
+                            Array.from({ length: pageCount }).map((_, index) => <li key={index}>
                                 <Link href={`/player/${id}/${index + 1}`} className={
                                     (pageNo === index + 1)
                                         ? "flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                                         : "flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                                } key={index}>{index + 1}</Link>
+                                }>{index + 1}</Link>
                             </li>)
                         }
-                        <li>
+                        <li key={"next"}>
                             <Link href={pageNo < pageCount ? `/player/${id}/${pageNo + 1}` : "#"} className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</Link>
                         </li>
                     </ul>
